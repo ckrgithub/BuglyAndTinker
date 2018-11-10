11884cd08c7b3326b6925cc88f636424f0c9e178
ckr <chenckr@77ircloud.com>
2018-11-10 10:08:09 +0800

    配置tinker_support.gradle

---
+++ app/src/main/java/com/ckr/upgrade/MyApplication.java
@@ -0,0 +1,37 @@
+package com.ckr.upgrade;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+
+import com.tencent.bugly.beta.Beta;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.entry.DefaultApplicationLike;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+/**
+ * Created by ckr on 2018/11/10.
+ */
+
+@SuppressWarnings("unused")
+@DefaultLifeCycle(application = "com.ckr.upgrade.CkrApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false)
+public class MyApplication extends DefaultApplicationLike {
+
+    public MyApplication(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        Beta.installTinker(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
