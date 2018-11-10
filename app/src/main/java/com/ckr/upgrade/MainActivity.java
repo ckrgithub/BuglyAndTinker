105640d784869dfee180208e6b0863d029f57d12
ckr <chenckr@77ircloud.com>
2018-11-10 11:43:30 +0800

    optimize

--- app/src/main/java/com/ckr/upgrade/MainActivity.java
+++ app/src/main/java/com/ckr/upgrade/MainActivity.java
@@ -2,12 +2,21 @@ package com.ckr.upgrade;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView versionView;
+    private TextView descriptionView;
+    private TextView thinkerIdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        descriptionView = findViewById(R.id.description);
+        versionView = findViewById(R.id.version);
+        thinkerIdView = findViewById(R.id.thinkerId);
     }
 }
