0c4d9f58f1901f259dfbcd9657c88bde31989729
ckr <chenckr@77ircloud.com>
2018-11-10 11:40:21 +0800

    完善bugly配置

--- app/src/main/java/com/ckr/upgrade/MyApplication.java
+++ app/src/main/java/com/ckr/upgrade/MyApplication.java
@@ -3,14 +3,21 @@ package com.ckr.upgrade;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Environment;
+import android.os.Process;
+import android.text.TextUtils;
 
+import com.ckr.walle.ChannelUtil;
 import com.tencent.bugly.Bugly;
 import com.tencent.bugly.beta.Beta;
+import com.tencent.bugly.crashreport.CrashReport;
 import com.tencent.tinker.anno.DefaultLifeCycle;
 import com.tencent.tinker.entry.DefaultApplicationLike;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by ckr on 2018/11/10.
@@ -36,8 +43,29 @@ public class MyApplication extends DefaultApplicationLike {
     @Override
     public void onCreate() {
         super.onCreate();
+        initBugly();
+    }
+
+    /**
+     * 应用升级和异常上报
+     */
+    private void initBugly() {
         Context context = getApplication().getApplicationContext();
-        //Beta类作为Bugly的初始化扩展，通过Beta类可以修改升级的检测时机，界面元素以及自定义升级行为
+
+        //<editor-fold desc="异常上报">
+        String packageName = context.getPackageName();//当前包名
+        String processName = getProcessName(Process.myPid());//当前进程名
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(context);
+        strategy.setUploadProcess(processName == null || processName.equals(packageName));//设置上报进程
+        strategy.setAppChannel(ChannelUtil.getChannelInfo(context));//设置渠道信息
+        strategy.setAppVersion(BuildConfig.VERSION_NAME);//app版本
+        strategy.setAppPackageName(packageName);//app包名
+        strategy.setAppReportDelay(10 * 1000);//Bugly在启动10s后联网同步数据
+//        CrashReport.setUserSceneTag(context,20181110);//设置标签，标明app的某个场景
+//        CrashReport.setIsDevelopmentDevice(context,BuildConfig.IS_DEBUG);//设置开发设备
+//      </editor-fold>
+
+        //<editor-fold desc="应用升级-->Beta类作为Bugly的初始化扩展，通过Beta类可以修改升级的检测时机，界面元素以及自定义升级行为">
         Beta.autoInit = true;//是否自动初始化升级模块
 //        Beta.init(context,BuildConfig.IS_DEBUG);//手动初始化升级模块
 //        Beta.upgradeCheckPeriod = 60 * 1000;//设置升级检查周期为60s,60s内sdk不重复向后台请求策略
@@ -49,10 +77,40 @@ public class MyApplication extends DefaultApplicationLike {
 //        Beta.showInterruptedStrategy = true;//设置点击过确认的弹窗 在app下次启动自动检查更新时会再次显示。
 //        Beta.canShowUpgradeActs.add(MainActivity.class);//添加可显示弹窗的Activity
         Beta.enableNotification = true;//设置是否显示消息通知
-        Beta.autoDownloadOnWifi=false;//设置wifi下自动下载
-        Beta.enableHotfix=true;//设置开启热更新
+        Beta.autoDownloadOnWifi = false;//设置wifi下自动下载
+        Beta.enableHotfix = true;//设置开启热更新
+//      </editor-fold>
 
-        Bugly.init(context, BUGLY_ID, BuildConfig.IS_DEBUG);
+        Bugly.init(context, BUGLY_ID, BuildConfig.IS_DEBUG, strategy);
+    }
+
+    /**
+     * 获取进程号对应的进程名
+     *
+     * @param pid 进程号
+     * @return 进程名
+     */
+    private static String getProcessName(int pid) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
+            String processName = reader.readLine();
+            if (!TextUtils.isEmpty(processName)) {
+                processName = processName.trim();
+            }
+            return processName;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+        return null;
     }
 
 }
