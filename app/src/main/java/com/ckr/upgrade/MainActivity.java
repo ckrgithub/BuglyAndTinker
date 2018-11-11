9995b8ddf670038a156d12073eeb5bd7ee1a3442
ckrgithub <13560177212@163.com>
2018-11-11 12:21:36 +0800

    判空处理

--- app/src/main/java/com/ckr/upgrade/MainActivity.java
+++ app/src/main/java/com/ckr/upgrade/MainActivity.java
@@ -2,9 +2,8 @@ package com.ckr.upgrade;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.tencent.bugly.beta.Beta;
@@ -14,28 +13,31 @@ import com.tencent.bugly.beta.tinker.TinkerManager;
 import static com.ckr.upgrade.UpgradeLog.Logd;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+	private static final String TAG = "MainActivity";
 
-    private TextView versionView;
-    private TextView tinkerIdView;
+	private TextView versionView;
+	private TextView tinkerIdView;
 
-    public static void start(Context context) {
-        Intent starter = new Intent(context, MainActivity.class);
-        context.startActivity(starter);
-    }
+	public static void start(Context context) {
+		Intent starter = new Intent(context, MainActivity.class);
+		context.startActivity(starter);
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        versionView = findViewById(R.id.version);
-        tinkerIdView = findViewById(R.id.tinkerId);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		versionView = findViewById(R.id.version);
+		tinkerIdView = findViewById(R.id.tinkerId);
 
-        UpgradeInfo upgradeInfo = Beta.getUpgradeInfo();
-        String versionName = upgradeInfo.versionName;
-        String tinkerId = TinkerManager.getTinkerId();
-        Logd(TAG, "onCreate: versionName:" + versionName+",tinkerId:"+tinkerId);
-        versionView.append(versionName);
-        tinkerIdView.append(tinkerId);
-    }
+		UpgradeInfo upgradeInfo = Beta.getUpgradeInfo();
+		String versionName = null;
+		if (upgradeInfo != null) {
+			versionName = upgradeInfo.versionName;
+			versionView.append(versionName);
+		}
+		String tinkerId = TinkerManager.getTinkerId();
+		Logd(TAG, "onCreate: versionName:" + versionName + ",tinkerId:" + tinkerId);
+		tinkerIdView.append(tinkerId);
+	}
 }
