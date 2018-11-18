b4a812e33ef8ac84aecf2665073c6877f09f3042
ckrgithub <13560177212@163.com>
2018-11-18 16:54:55 +0800

    调用starDownload前，判断UpgradeInfo不为null

--- app/src/main/java/com/ckr/upgrade/MainActivity.java
+++ app/src/main/java/com/ckr/upgrade/MainActivity.java
@@ -2,7 +2,6 @@ package com.ckr.upgrade;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.view.View;
@@ -10,89 +9,90 @@ import android.widget.TextView;
 
 import com.ckr.upgrade.util.DownloadManager;
 import com.tencent.bugly.beta.Beta;
-import com.tencent.bugly.beta.UpgradeInfo;
 import com.tencent.bugly.beta.tinker.TinkerManager;
 
 import static com.ckr.upgrade.util.UpgradeLog.Logd;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
-    private static final String TAG = "MainActivity";
+	private static final String TAG = "MainActivity";
 
-    private TextView versionView;
-    private TextView tinkerIdView;
-    private View checkUpgrade;
-    private Intent intent;
+	private TextView versionView;
+	private TextView tinkerIdView;
+	private View checkUpgrade;
+	private Intent intent;
 
-    public static void start(Context context) {
-        Intent starter = new Intent(context, MainActivity.class);
-        context.startActivity(starter);
-    }
+	public static void start(Context context) {
+		Intent starter = new Intent(context, MainActivity.class);
+		context.startActivity(starter);
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        versionView = findViewById(R.id.version);
-        tinkerIdView = findViewById(R.id.tinkerId);
-        checkUpgrade = findViewById(R.id.checkUpgrade);
-        checkUpgrade.setOnClickListener(this);
-        setOnClickListener(R.id.btnService);
-        setOnClickListener(R.id.btnReceiver);
-        setOnClickListener(R.id.btnNotification);
-        setOnClickListener(R.id.btnDownload);
-        setOnClickListener(R.id.btnPause);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		versionView = findViewById(R.id.version);
+		tinkerIdView = findViewById(R.id.tinkerId);
+		checkUpgrade = findViewById(R.id.checkUpgrade);
+		checkUpgrade.setOnClickListener(this);
+		setOnClickListener(R.id.btnService);
+		setOnClickListener(R.id.btnReceiver);
+		setOnClickListener(R.id.btnNotification);
+		setOnClickListener(R.id.btnDownload);
+		setOnClickListener(R.id.btnPause);
 
-        versionView.append(BuildConfig.VERSION_NAME + "-" + BuildConfig.VERSION_CODE);
-        String tinkerId = TinkerManager.getTinkerId();
-        Logd(TAG, "onCreate: tinkerId:" + tinkerId);
-        tinkerIdView.append(tinkerId);
-    }
+		versionView.append(BuildConfig.VERSION_NAME + "-" + BuildConfig.VERSION_CODE);
+		String tinkerId = TinkerManager.getTinkerId();
+		Logd(TAG, "onCreate: tinkerId:" + tinkerId);
+		tinkerIdView.append(tinkerId);
+	}
 
-    private void setOnClickListener(@IdRes int viewId) {
-        findViewById(viewId).setOnClickListener(this);
-    }
+	private void setOnClickListener(@IdRes int viewId) {
+		findViewById(viewId).setOnClickListener(this);
+	}
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (intent != null) {
-            stopService(intent);
-        }
-    }
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (intent != null) {
+			stopService(intent);
+		}
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.checkUpgrade:
-                Logd(TAG, "onClick: 检查更新");
-                Beta.checkUpgrade(false, false);
-                break;
-            case R.id.btnService:
-                intent = new Intent(this, DownLoadService.class);
-                startService(intent);
-                break;
-            case R.id.btnReceiver:
-                break;
-            case R.id.btnNotification:
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        DownloadManager with = DownloadManager.with(MainActivity.this.getApplicationContext());
-                        with.sendNotification();
-                    }
-                }).start();
-                break;
-            case R.id.btnDownload:
-                DownloadManager with = DownloadManager.with(this.getApplicationContext());
-                if (with.getDownloadStatus() == DownloadManager.PAUSED) {
-                    with.resumeDownload();
-                } else {
-                    with.startDownload();
-                }
-                break;
-            case R.id.btnPause:
-                DownloadManager.with(this.getApplicationContext()).pauseDownload();
-                break;
-        }
-    }
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.checkUpgrade:
+				Logd(TAG, "onClick: 检查更新");
+				Beta.checkUpgrade(false, false);
+				break;
+			case R.id.btnService:
+				intent = new Intent(this, DownLoadService.class);
+				startService(intent);
+				break;
+			case R.id.btnReceiver:
+				break;
+			case R.id.btnNotification:
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						DownloadManager with = DownloadManager.with(MainActivity.this.getApplicationContext());
+						with.sendNotification();
+					}
+				}).start();
+				break;
+			case R.id.btnDownload:
+				DownloadManager with = DownloadManager.with(this.getApplicationContext());
+				if (with.getDownloadStatus() == DownloadManager.PAUSED) {
+					with.resumeDownload();
+				} else {
+					if (with.getUpgradeInfo() == null) {
+						with.startDownload();
+					}
+				}
+				break;
+			case R.id.btnPause:
+				DownloadManager.with(this.getApplicationContext()).pauseDownload();
+				break;
+		}
+	}
 }
