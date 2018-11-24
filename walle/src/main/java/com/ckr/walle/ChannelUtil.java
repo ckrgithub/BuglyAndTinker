e407df8e8dc2676c974371d87ad8082d7d2f4ed1
ckrgithub <13560177212@163.com>
2018-11-24 12:14:22 +0800

    context判空处理

--- walle/src/main/java/com/ckr/walle/ChannelUtil.java
+++ walle/src/main/java/com/ckr/walle/ChannelUtil.java
@@ -18,96 +18,109 @@ import java.io.IOException;
  */
 
 public class ChannelUtil {
-    private static final String TAG = "ChannelUtil";
-    private static final String DEFAULT_CHANNEL = "ckr";
+	private static final String TAG = "ChannelUtil";
 
-    /**
-     * 获取当前apk的路径
-     *
-     * @param context
-     * @return
-     */
-    private static String getSourceDir(Context context) {
-        ApplicationInfo applicationInfo = context.getApplicationInfo();
-        if (applicationInfo == null) {
-            return null;
-        }
-        return applicationInfo.sourceDir;
-    }
+	/**
+	 * 获取当前apk的路径
+	 *
+	 * @param context
+	 * @return
+	 */
+	private static String getSourceDir(Context context) {
+		ApplicationInfo applicationInfo = context.getApplicationInfo();
+		if (applicationInfo != null) {
+			return applicationInfo.sourceDir;
+		}
+		return null;
+	}
+
+	/**
+	 * 判空
+	 *
+	 * @param context
+	 */
+	private static void checkIsNull(Context context) {
+		if (context == null) {
+			throw new NullPointerException("context is null");
+		}
+	}
+
+	/**
+	 * 获取当前app渠道信息
+	 *
+	 * @param context
+	 * @param
+	 * @return
+	 */
+	public static String getChannelInfo(Context context) {
+		checkIsNull(context);
+		return getChannelInfo(context, getSourceDir(context));
+	}
 
 
-    /**
-     * 获取当前app渠道信息
-     *
-     * @param context
-     * @param
-     * @return
-     */
-    public static String getChannelInfo(Context context) {
-        return getChannelInfo(getSourceDir(context));
-    }
+	/**
+	 * 获取apk渠道信息
+	 *
+	 * @param context
+	 * @param apkPath apk路径
+	 * @return
+	 */
+	public static String getChannelInfo(Context context, String apkPath) {
+		Log.d(TAG, "getChannelInfo: apkPath:" + apkPath);
+		checkIsNull(context);
+		String channel = context.getString(R.string.upgrade_default_channel);
+		if (TextUtils.isEmpty(apkPath)) {
+			return channel;
+		}
+		ChannelInfo info = ChannelReader.get(new File(apkPath));
+		if (info != null) {
+			channel = info.getChannel();
+		}
+		return channel;
+	}
 
-    /**
-     * 获取apk渠道信息
-     *
-     * @param apkPath apk路径
-     * @return
-     */
-    public static String getChannelInfo(String apkPath) {
-        Log.d(TAG, "getChannelInfo: apkPath:" + apkPath);
-        String channel = DEFAULT_CHANNEL;
-        if (TextUtils.isEmpty(apkPath)) {
-            return channel;
-        }
-        ChannelInfo info = ChannelReader.get(new File(apkPath));
-        if (info != null) {
-            channel = info.getChannel();
-        }
-        return channel;
-    }
+	/**
+	 * 写入渠道信息
+	 *
+	 * @param apkPath     apk路径
+	 * @param channelInfo 渠道信息
+	 * @param isReplace   是否覆盖渠道信息
+	 * @return
+	 */
+	public static boolean setChannelInfo(String apkPath, String channelInfo, boolean isReplace) {
+		Log.d(TAG, "setChannelInfo: apkPath:" + apkPath + ",channelInfo:" + channelInfo + ",isReplace:" + isReplace);
+		if (TextUtils.isEmpty(apkPath)) {
+			return false;
+		}
+		return setChannelInfo(new File(apkPath), channelInfo, isReplace);
+	}
 
-    /**
-     * 写入渠道信息
-     *
-     * @param apkPath     apk路径
-     * @param channelInfo 渠道信息
-     * @param isReplace   是否覆盖渠道信息
-     * @return
-     */
-    public static boolean setChannelInfo(String apkPath, String channelInfo, boolean isReplace) {
-        Log.d(TAG, "setChannelInfo: apkPath:" + apkPath + ",channelInfo:" + channelInfo + ",isReplace:" + isReplace);
-        if (TextUtils.isEmpty(apkPath)) {
-            return false;
-        }
-        return setChannelInfo(new File(apkPath), channelInfo, isReplace);
-    }
-
-    /**
-     * 写入渠道信息
-     *
-     * @param apkFile     文件
-     * @param channelInfo 渠道信息
-     * @param isReplace   是否覆盖渠道信息
-     * @return
-     */
-    public static boolean setChannelInfo(File apkFile, String channelInfo, boolean isReplace) {
-        Log.d(TAG, "setChannelInfo ,channelInfo:" + channelInfo + ",isReplace:" + isReplace);
-        if (apkFile == null) {
-            return false;
-        }
-        try {
-            if (isReplace) {
-                ChannelWriter.remove(apkFile);
-            }
-            ChannelWriter.put(apkFile, channelInfo);
-            return true;
-        } catch (IOException e) {
-            Log.d(TAG, "setChannelInfo IOException: ");
-            e.printStackTrace();
-        } catch (SignatureNotFoundException e) {
-            Log.d(TAG, "setChannelInfo SignatureNotFoundException: ");
-            e.printStackTrace();
-        }
-        return false;
-    }
+	/**
+	 * 写入渠道信息
+	 *
+	 * @param apkFile     文件
+	 * @param channelInfo 渠道信息
+	 * @param isReplace   是否覆盖渠道信息
+	 * @return
+	 */
+	public static boolean setChannelInfo(File apkFile, String channelInfo, boolean isReplace) {
+		Log.d(TAG, "setChannelInfo ,channelInfo:" + channelInfo + ",isReplace:" + isReplace);
+		if (apkFile == null) {
+			return false;
+		}
+		try {
+			if (isReplace) {
+				ChannelWriter.remove(apkFile);
+			}
+			ChannelWriter.put(apkFile, channelInfo);
+			return true;
+		} catch (IOException e) {
+			Log.d(TAG, "setChannelInfo IOException: ");
+			e.printStackTrace();
+		} catch (SignatureNotFoundException e) {
+			Log.d(TAG, "setChannelInfo SignatureNotFoundException: ");
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
