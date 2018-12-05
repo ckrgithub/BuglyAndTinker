a48e953294f6515249c5090d31b89b207aa49f97
ckr <chenckr@77ircloud.com>
2018-12-05 11:34:52 +0800

    添加apk删除功能

---
+++ upgrade/src/main/java/com/ckr/upgrade/receiver/ApkInstallReceiver.java
@@ -0,0 +1,40 @@
+package com.ckr.upgrade.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+
+import com.ckr.upgrade.util.ApkUtil;
+
+import java.io.File;
+
+import static com.ckr.upgrade.DownloadManager.APK_URL;
+import static com.ckr.upgrade.DownloadManager.FILE_NAME;
+import static com.ckr.upgrade.util.UpgradeLog.Logd;
+
+/**
+ * Created by ckr on 2018/12/5.
+ */
+
+public class ApkInstallReceiver extends BroadcastReceiver {
+    private static final String TAG = "ApkInstallReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_PACKAGE_ADDED)) {
+            Logd(TAG, "onReceive: 安装成功");
+            SharedPreferences preferences = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+            String saveApkUrl = preferences.getString(APK_URL, null);
+            if (!TextUtils.isEmpty(saveApkUrl)) {
+                final String path = ApkUtil.getApkPath(saveApkUrl, context);
+                final File file = new File(path);
+                if (file.exists()) {
+                    boolean delete = file.delete();
+                    Logd(TAG, "onReceive: delete:"+delete);
+                }
+            }
+        }
+    }
+}
