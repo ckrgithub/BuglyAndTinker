797eb9c0a3486f8badfbf16e36962b0fc0bed926
ckrgithub <13560177212@163.com>
2018-12-17 23:04:39 +0800

    优化

--- upgrade/src/main/java/com/ckr/upgrade/listener/ApkUpgradeListener.java
+++ upgrade/src/main/java/com/ckr/upgrade/listener/ApkUpgradeListener.java
@@ -1,18 +1,14 @@
 package com.ckr.upgrade.listener;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.text.TextUtils;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.ckr.upgrade.DownloadManager;
 import com.ckr.upgrade.R;
 import com.ckr.upgrade.UpgradeConfig;
 import com.ckr.upgrade.UpgradeManager;
-import com.ckr.upgrade.util.ApkUtil;
 import com.ckr.upgrade.util.AppTracker;
 import com.ckr.upgrade.util.UpgradeLog;
 import com.tencent.bugly.beta.UpgradeInfo;
@@ -25,46 +21,47 @@ import static com.ckr.upgrade.util.UpgradeLog.Logd;
  */
 
 public class ApkUpgradeListener implements UpgradeListener, Runnable {
-    private static final String TAG = "ApkUpgradeListener";
+	private static final String TAG = "ApkUpgradeListener";
 
-    private final Context context;
+	private final Context context;
 
-    public ApkUpgradeListener(@NonNull Context context) {
-        this.context = context;
-    }
+	public ApkUpgradeListener(@NonNull Context context) {
+		this.context = context;
+	}
 
-    @Override
-    public void onUpgrade(int i, UpgradeInfo upgradeInfo, boolean b, boolean b1) {
-        Logd(TAG, "onUpgrade: i" + i + ",b:" + b + ",b1:" + b1 + ",upgradeInfo:" + upgradeInfo);
-        if (upgradeInfo != null) {
-            com.ckr.upgrade.UpgradeInfo info = new com.ckr.upgrade.UpgradeInfo();
-            info.title = upgradeInfo.title;
-            info.newFeature = upgradeInfo.newFeature;
-            info.upgradeType = upgradeInfo.upgradeType;
-            info.versionCode = upgradeInfo.versionCode;
-            info.versionName = upgradeInfo.versionName;
-            info.apkUrl = upgradeInfo.apkUrl;
-            info.fileSize = upgradeInfo.fileSize;
-            info.popTimes = upgradeInfo.popTimes;
-            info.popInterval = upgradeInfo.popInterval;
-            DownloadManager.with(context).setUpgradeInfo(info);
-            new Handler().postDelayed(this, 500);
-        } else {
-            UpgradeLog.Loge(TAG, "onUpgrade: 不需要更新，没有更新策略");
-            if (UpgradeConfig.toastWhenNoUpgrade) {
-                if (context != null) {
-                    Toast.makeText(context, context.getString(R.string.tips_no_upgrade), Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
+	@Override
+	public void onUpgrade(int i, UpgradeInfo upgradeInfo, boolean b, boolean b1) {
+		Logd(TAG, "onUpgrade: i" + i + ",b:" + b + ",b1:" + b1 + ",upgradeInfo:" + upgradeInfo);
+		if (upgradeInfo != null) {
+			com.ckr.upgrade.UpgradeInfo info = new com.ckr.upgrade.UpgradeInfo();
+			info.title = upgradeInfo.title;
+			info.newFeature = upgradeInfo.newFeature;
+			info.upgradeType = upgradeInfo.upgradeType;
+			info.versionCode = upgradeInfo.versionCode;
+			info.versionName = upgradeInfo.versionName;
+			info.apkUrl = upgradeInfo.apkUrl;
+			info.fileSize = upgradeInfo.fileSize;
+			info.popTimes = upgradeInfo.popTimes;
+			info.popInterval = upgradeInfo.popInterval;
+			DownloadManager.with(context).setUpgradeInfo(info);
+			new Handler().postDelayed(this, 500);
+		} else {
+			UpgradeLog.Loge(TAG, "onUpgrade: 不需要更新，没有更新策略");
+			if (UpgradeConfig.toastWhenNoUpgrade) {
+				UpgradeConfig.toastWhenNoUpgrade = false;
+				if (context != null) {
+					Toast.makeText(context, context.getString(R.string.tips_no_upgrade), Toast.LENGTH_SHORT).show();
+				}
+			}
+		}
+	}
 
-    @Override
-    public void run() {
-        UpgradeLog.Loge(TAG, "run: 需要更新，存在更新策略");
-        AppTracker appTracker = UpgradeManager.getAppTracker();
-        if (appTracker != null) {
-            appTracker.showDialog(true);
-        }
-    }
+	@Override
+	public void run() {
+		UpgradeLog.Loge(TAG, "run: 需要更新，存在更新策略");
+		AppTracker appTracker = UpgradeManager.getAppTracker();
+		if (appTracker != null) {
+			appTracker.showDialog(true);
+		}
+	}
 }
