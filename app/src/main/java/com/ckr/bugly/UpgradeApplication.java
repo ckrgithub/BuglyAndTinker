8e42b4903f39af5119112aad3ea4d81f78173e49
ckr <chenckr@77ircloud.com>
2018-12-21 14:51:37 +0800

    优化

--- app/src/main/java/com/ckr/bugly/UpgradeApplication.java
+++ app/src/main/java/com/ckr/bugly/UpgradeApplication.java
@@ -10,34 +10,13 @@ import com.ckr.walle.ChannelUtil;
 /**
  * Created by ckr on 2018/11/10.
  */
-//@SuppressWarnings("unused")
-//@DefaultLifeCycle(application = "com.ckr.bugly.CkrApplication",
-//        flags = ShareConstants.TINKER_ENABLE_ALL,
-//        loadVerifyFlag = false)
 public class UpgradeApplication extends Application {
-    private static UpgradeApplication instance;
     private static final String BUGLY_ID = "83ffe4ff10";
-
-    public static UpgradeApplication getInstance() {
-        return instance;
-    }
-
-//    public UpgradeApplication(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
-//        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
-//    }
-//
-//    @Override
-//    public void onBaseContextAttached(Context base) {
-//        super.onBaseContextAttached(base);
-//        //热更新配置
-//        Beta.installTinker(this);
-//    }
 
     @Override
     public void onCreate() {
         super.onCreate();
         UpgradeLog.debug(BuildConfig.IS_DEBUG);
-        instance = this;
         UpgradeConfig.isDebug = BuildConfig.IS_DEBUG;
         UpgradeConfig.isAutoInstall = true;
         UpgradeConfig.enableNotification = true;
