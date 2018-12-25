659dba94600df39dd9cf65b84e14ffd2fa4715ed
ckr <chenckr@77ircloud.com>
2018-12-25 17:23:42 +0800

    可配置dialog取消类型

--- upgrade/src/main/java/com/ckr/upgrade/dialog/UpgradeDialogFragment.java
+++ upgrade/src/main/java/com/ckr/upgrade/dialog/UpgradeDialogFragment.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.FragmentManager;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
