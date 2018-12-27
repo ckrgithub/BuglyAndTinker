923a75465b247d28dd2226ae9ebdf9300ae04237
ckr <chenckr@77ircloud.com>
2018-12-27 13:53:31 +0800

    修复OkHttp3证书认证

---
+++ upgrade/src/main/java/com/ckr/upgrade/util/SSLSocketClient.java
@@ -0,0 +1,63 @@
+package com.ckr.upgrade.util;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * Created by ckr on 2018/12/27.
+ * 查看链接：https://blog.csdn.net/u010982507/article/details/85258477
+ */
+
+public class SSLSocketClient {
+    //获取这个SSLSocketFactory
+    public static SSLSocketFactory getSSLSocketFactory() {
+        try {
+            SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, getTrustManager(), new SecureRandom());
+            return sslContext.getSocketFactory();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //获取TrustManager
+    private static TrustManager[] getTrustManager() {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                    }
+
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        // return null; 或者
+                        return new X509Certificate[]{}; // 空实现
+                    }
+                }
+        };
+        return trustAllCerts;
+    }
+
+    //获取HostnameVerifier
+    public static HostnameVerifier getHostnameVerifier() {
+        HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+            @Override
+            public boolean verify(String s, SSLSession sslSession) {
+                // true表示信任所有域名
+                return true;
+            }
+        };
+        return hostnameVerifier;
+    }
+}
