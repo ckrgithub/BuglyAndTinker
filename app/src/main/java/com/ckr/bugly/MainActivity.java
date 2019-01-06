788f95f2b5e33abbeffe18b8e6c5818975c166af
ckrgithub <13560177212@163.com>
2019-01-06 20:51:42 +0800

    添加补丁检测按钮

--- app/src/main/java/com/ckr/bugly/MainActivity.java
+++ app/src/main/java/com/ckr/bugly/MainActivity.java
@@ -15,6 +15,7 @@ import com.ckr.upgrade.listener.OnInstallApkListener;
 import com.ckr.upgrade.util.ApkUtil;
 import com.tencent.bugly.beta.Beta;
 import com.tencent.bugly.beta.tinker.TinkerManager;
+import com.tencent.tinker.lib.tinker.Tinker;
 
 import java.io.File;
 
@@ -23,138 +24,152 @@ import static com.ckr.upgrade.util.ApkUtil.REQUEST_CODE_INSTALL;
 import static com.ckr.upgrade.util.UpgradeLog.Logd;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener, OnInstallApkListener {
-    private static final String TAG = "MainActivity";
-    private TextView versionView;
-    private TextView tinkerIdView;
-    private View checkUpgrade;
-    private Intent intent;
+	private static final String TAG = "MainActivity";
+	private TextView versionView;
+	private TextView tinkerIdView;
+	private View checkUpgrade;
+	private Intent intent;
 
-    public static void start(Context context) {
-        Intent starter = new Intent(context, MainActivity.class);
-        context.startActivity(starter);
-    }
+	public static void start(Context context) {
+		Intent starter = new Intent(context, MainActivity.class);
+		context.startActivity(starter);
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        versionView = findViewById(R.id.version);
-        tinkerIdView = findViewById(R.id.tinkerId);
-        checkUpgrade = findViewById(R.id.checkUpgrade);
-        checkUpgrade.setOnClickListener(this);
-        setOnClickListener(R.id.btnService);
-        setOnClickListener(R.id.btnInstaller);
-        setOnClickListener(R.id.btnNotification);
-        setOnClickListener(R.id.btnDownload);
-        setOnClickListener(R.id.btnPause);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		versionView = findViewById(R.id.version);
+		tinkerIdView = findViewById(R.id.tinkerId);
+		checkUpgrade = findViewById(R.id.checkUpgrade);
+		checkUpgrade.setOnClickListener(this);
+		setOnClickListener(R.id.btnService);
+		setOnClickListener(R.id.btnInstaller);
+		setOnClickListener(R.id.btnNotification);
+		setOnClickListener(R.id.btnDownload);
+		setOnClickListener(R.id.btnPause);
+		setOnClickListener(R.id.btnPatch);
 
-        versionView.append(BuildConfig.VERSION_NAME + "-" + BuildConfig.VERSION_CODE);
-        String tinkerId = TinkerManager.getTinkerId();
-        Logd(TAG, "onCreate: tinkerId:" + tinkerId);
-        tinkerIdView.append(tinkerId);
-    }
+		versionView.append(BuildConfig.VERSION_NAME + "-" + BuildConfig.VERSION_CODE);
+		String tinkerId = TinkerManager.getTinkerId();
+		Logd(TAG, "onCreate: tinkerId:" + tinkerId);
+		tinkerIdView.append(tinkerId);
+	}
 
-    private void setOnClickListener(@IdRes int viewId) {
-        findViewById(viewId).setOnClickListener(this);
-    }
+	private void setOnClickListener(@IdRes int viewId) {
+		findViewById(viewId).setOnClickListener(this);
+	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Logd(TAG, "onActivityResult: requestCode:" + requestCode + ",resultCode:" + resultCode);
-        if (requestCode == REQUEST_CODE_INSTALL) {
-            Context context = getContext();
-            boolean canInstall = ApkUtil.hasInstallPermission(context);
-            if (canInstall) {
-                DownloadManager with = DownloadManager.with(context);
-                UpgradeInfo upgradeInfo = with.getUpgradeInfo();
-                if (upgradeInfo != null) {
-                    int downloadStatus = with.getDownloadStatus();
-                    if (downloadStatus == COMPLETE) {
-                        ApkUtil.installApk(ApkUtil.getApkPath(upgradeInfo.apkUrl, context), this,false);
-                    }
-                }
-            }
-        }
-    }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Logd(TAG, "onActivityResult: requestCode:" + requestCode + ",resultCode:" + resultCode);
+		if (requestCode == REQUEST_CODE_INSTALL) {
+			Context context = getContext();
+			boolean canInstall = ApkUtil.hasInstallPermission(context);
+			if (canInstall) {
+				DownloadManager with = DownloadManager.with(context);
+				UpgradeInfo upgradeInfo = with.getUpgradeInfo();
+				if (upgradeInfo != null) {
+					int downloadStatus = with.getDownloadStatus();
+					if (downloadStatus == COMPLETE) {
+						ApkUtil.installApk(ApkUtil.getApkPath(upgradeInfo.apkUrl, context), this, false);
+					}
+				}
+			}
+		}
+	}
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (intent != null) {
-            stopService(intent);
-        }
-    }
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (intent != null) {
+			stopService(intent);
+		}
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.checkUpgrade:
-                Logd(TAG, "onClick: 检查更新");
-                Beta.checkUpgrade(true, true);
-                break;
-            case R.id.btnService:
-                intent = new Intent(this, DownLoadService.class);
-                startService(intent);
-                break;
-            case R.id.btnInstaller:
-                boolean canInstall = true;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                    canInstall = getPackageManager().canRequestPackageInstalls();
-                }
-                if (!canInstall) {
-                    Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
-                    Uri uri = Uri.fromParts("package", getPackageName(), null);
-                    intent.setData(uri);
-                    startActivityForResult(intent, REQUEST_CODE_INSTALL);
-                } else {
-                    DownloadManager downloadManager = DownloadManager.with(this);
-                    UpgradeInfo upgradeInfo = downloadManager.getUpgradeInfo();
-                    if (upgradeInfo != null) {
-                        String apkUrl = upgradeInfo.apkUrl;
-                        String path = ApkUtil.getApkPath(apkUrl, this);
-                        int downloadStatus = downloadManager.getDownloadStatus();
-                        boolean isComplete = downloadStatus == COMPLETE;
-                        if (!isComplete) {
-                            long fileSize = upgradeInfo.fileSize;
-                            File file = new File(path);
-                            if (file.length() >= fileSize) {
-                                isComplete = true;
-                            }
-                        }
-                        if (isComplete) {
-                            ApkUtil.installApk(path, this,true);
-                        }
-                    }
-                }
-                break;
-            case R.id.btnNotification:
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        DownloadManager with = DownloadManager.with(MainActivity.this.getApplicationContext());
-                        with.sendNotification();
-                    }
-                }).start();
-                break;
-            case R.id.btnDownload:
-                DownloadManager with = DownloadManager.with(this.getApplicationContext());
-                if (with.getDownloadStatus() == DownloadManager.PAUSED) {
-                    with.resumeDownload();
-                } else {
-                    if (with.getUpgradeInfo() == null) {
-                        with.startDownload();
-                    }
-                }
-                break;
-            case R.id.btnPause:
-                DownloadManager.with(this.getApplicationContext()).pauseDownload();
-                break;
-        }
-    }
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.checkUpgrade:
+				Logd(TAG, "onClick: 检查更新");
+				Beta.checkUpgrade(true, true);
+				break;
+			case R.id.btnService:
+				intent = new Intent(this, DownLoadService.class);
+				startService(intent);
+				break;
+			case R.id.btnInstaller:
+				boolean canInstall = true;
+				if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+					canInstall = getPackageManager().canRequestPackageInstalls();
+				}
+				if (!canInstall) {
+					Intent intent = new Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES);
+					Uri uri = Uri.fromParts("package", getPackageName(), null);
+					intent.setData(uri);
+					startActivityForResult(intent, REQUEST_CODE_INSTALL);
+				} else {
+					DownloadManager downloadManager = DownloadManager.with(this);
+					UpgradeInfo upgradeInfo = downloadManager.getUpgradeInfo();
+					if (upgradeInfo != null) {
+						String apkUrl = upgradeInfo.apkUrl;
+						String path = ApkUtil.getApkPath(apkUrl, this);
+						int downloadStatus = downloadManager.getDownloadStatus();
+						boolean isComplete = downloadStatus == COMPLETE;
+						if (!isComplete) {
+							long fileSize = upgradeInfo.fileSize;
+							File file = new File(path);
+							if (file.length() >= fileSize) {
+								isComplete = true;
+							}
+						}
+						if (isComplete) {
+							ApkUtil.installApk(path, this, true);
+						}
+					}
+				}
+				break;
+			case R.id.btnNotification:
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						DownloadManager with = DownloadManager.with(MainActivity.this.getApplicationContext());
+						with.sendNotification();
+					}
+				}).start();
+				break;
+			case R.id.btnDownload:
+				DownloadManager with = DownloadManager.with(this.getApplicationContext());
+				if (with.getDownloadStatus() == DownloadManager.PAUSED) {
+					with.resumeDownload();
+				} else {
+					if (with.getUpgradeInfo() == null) {
+						with.startDownload();
+					}
+				}
+				break;
+			case R.id.btnPause:
+				DownloadManager.with(this.getApplicationContext()).pauseDownload();
+				break;
+			case R.id.btnPatch:
+				Tinker tinker = Tinker.with(this.getApplicationContext());
+				boolean tinkerEnabled = tinker.isTinkerEnabled();
+				boolean tinkerLoaded = tinker.isTinkerLoaded();
+				boolean enableHotfix = Beta.enableHotfix;
+				Logd(TAG, "onClick: tinkerEnabled:" + tinkerEnabled + ",tinkerLoaded:" + tinkerLoaded + ",enableHotfix:" + enableHotfix);
+				String newTinkerId = TinkerManager.getNewTinkerId();
+				String tinkerId = TinkerManager.getTinkerId();
+				Logd(TAG, "onClick: newTinkerId:" + newTinkerId + ",tinkerId:" + tinkerId);
+				if (tinkerEnabled && !tinkerLoaded && enableHotfix) {
+					Beta.downloadPatch();
+				}
+				break;
+		}
+	}
 
-    @Override
-    public void requestInstallPermission() {
-        ApkUtil.openUnknownAppSourcesActivity(this);
-    }
+	@Override
+	public void requestInstallPermission() {
+		ApkUtil.openUnknownAppSourcesActivity(this);
+	}
 }
--- app/src/main/java/com/ckr/bugly/UpgradeApplication.java
+++ app/src/main/java/com/ckr/bugly/UpgradeApplication.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 
 import com.ckr.upgrade.UpgradeConfig;
 import com.ckr.upgrade.UpgradeManager;
-import com.ckr.upgrade.util.UpgradeLog;
 import com.ckr.walle.ChannelUtil;
 import com.tencent.bugly.beta.Beta;
 import com.tencent.tinker.anno.DefaultLifeCycle;
@@ -18,41 +17,40 @@ import com.tencent.tinker.loader.shareutil.ShareConstants;
  */
 @SuppressWarnings("unused")
 @DefaultLifeCycle(application = "com.ckr.bugly.CkrApplication",
-        flags = ShareConstants.TINKER_ENABLE_ALL,
-        loadVerifyFlag = false)
+		flags = ShareConstants.TINKER_ENABLE_ALL,
+		loadVerifyFlag = false)
 public class UpgradeApplication extends DefaultApplicationLike {
-    private static UpgradeApplication instance;
-    private static final String BUGLY_ID = "83ffe4ff10";
+	private static UpgradeApplication instance;
+	private static final String BUGLY_ID = "83ffe4ff10";
 
-    public static UpgradeApplication getInstance() {
-        return instance;
-    }
+	public static UpgradeApplication getInstance() {
+		return instance;
+	}
 
-    public UpgradeApplication(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
-        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
-    }
+	public UpgradeApplication(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+		super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+	}
 
-    @Override
-    public void onBaseContextAttached(Context base) {
-        super.onBaseContextAttached(base);
-        //热更新配置
-        Beta.installTinker(this);
-    }
+	@Override
+	public void onBaseContextAttached(Context base) {
+		super.onBaseContextAttached(base);
+		//热更新配置
+		Beta.installTinker(this);
+	}
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        UpgradeLog.debug(BuildConfig.IS_DEBUG);
-        instance = this;
-        UpgradeConfig.isDebug = BuildConfig.IS_DEBUG;
-        UpgradeConfig.isAutoInstall = true;
-        UpgradeConfig.enableNotification = true;
-        UpgradeConfig.enableWriteChannelInfo = true;
-        UpgradeConfig.canShowUpgradeActs.add(MainActivity.class);
-        UpgradeConfig upgradeConfig = new UpgradeConfig(BUGLY_ID, BuildConfig.VERSION_NAME, ChannelUtil.getChannelInfo(this.getApplication().getApplicationContext()), R.mipmap.ic_launcher);
-        //升级功能配置
-        UpgradeManager.init(getApplication(), upgradeConfig,true);
-    }
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		instance = this;
+		UpgradeConfig.isDebug = BuildConfig.IS_DEBUG;
+		UpgradeConfig.isAutoInstall = true;
+		UpgradeConfig.enableNotification = true;
+		UpgradeConfig.enableWriteChannelInfo = true;
+		UpgradeConfig.canShowUpgradeActs.add(MainActivity.class);
+		UpgradeConfig upgradeConfig = new UpgradeConfig(BUGLY_ID, BuildConfig.VERSION_NAME, ChannelUtil.getChannelInfo(this.getApplication().getApplicationContext()), R.mipmap.ic_launcher);
+		//升级功能配置
+		UpgradeManager.init(getApplication(), upgradeConfig, true);
+	}
 
 
 }
